d4c121497db0ff0abf6d6b0af3303d92a87e243d
Rob Lang <brainwiped@gmail.com>
2018-11-03 07:04:08 +0000

    First commit - simplistic version of core code

---
+++ BloggerToMd/Program.cs
@@ -0,0 +1,19 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggerToMd
+{
+    class Program
+    {
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Option("-src")]
+        [Required]
+        public string Source { get; }
+
+        public void OnExecute()
+        {
+            BloggerToMarkdown.Convert(Source);
+        }
+    }
+}
